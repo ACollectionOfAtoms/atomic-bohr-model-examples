b0da715998f3566db84b4edb38177da5dba51b6a
Adam Hernandez <hernandezdavid88@utexas.edu>
2016-10-01 15:10:31 -0500

    Added modal with atomic info

--- periodic-table/index.js
+++ periodic-table/index.js
@@ -8,7 +8,15 @@ var atomConfig = {  //All atoms share this base-config
   nucleusColor: 'rgba(3, 169, 244, 0.2)',
   electronColor: 'rgba(24, 255, 255, 0.6)'
 }
-var speed = 80, // how fast the atoms come into existence
+
+var glow = function($el) {
+  $el.addClass('box-glow')
+  setTimeout(function() {
+    $el.removeClass('box-glow')
+  }, 500)
+}
+
+var speed = 180, // how fast the atoms come into existence
     myAtoms = []
 
 for (var i=1; i <= 118; i++) { // iterate over all existing atoms
@@ -19,13 +27,22 @@ for (var i=1; i <= 118; i++) { // iterate over all existing atoms
       atomConfig.numElectrons = i
       atomConfig.idNumber = i
       atomConfig.containerId = `#bohr-container-${i}`
-      // $(`#bohr-container-${i}`).append(`<div class='tooltip' id='tooltip-id-${i}'>`)
-      myAtoms.push(new Atom(atomConfig))
-      // $(`#tooltip-id-${i}`).append(`<span class="tooltiptext" id='tooltip-text-${i}'>`)
-      // $(`#tooltip-text-${i}`).text(myAtoms[myAtoms.length -1].wikiSummary)
+      var $container = $(`#bohr-container-${i}`)
+      var newAtom = new Atom(atomConfig)
+      var $atom = $(`#atom-${i}`)
+      $atom.addClass('clickable-atom')
+      newAtom.infoFunc = $atom.click(function() {
+        $atom.attr('data-toggle', 'modal')
+        $atom.attr('data-target', '.bs-example-modal-lg')
+        $('.modal-title').text(newAtom.elementName)
+        $('.wiki-url').text(newAtom.wikiUrl)
+        $('.wiki-summary').text(newAtom.wikiSummary)
+      })
+      myAtoms.push(newAtom)
     }, i * speed);
   }(i));
 }
+// TODO: Add info button about animation; modals with atomic info
 function getRandomInt(min, max) { // to randomly assign rotational pattern and alternating bool
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -41,7 +58,7 @@ var rotationalPatterns = [
   ],
   orbitalRotationConfig = {pattern:{preset:''}},
   alternate = [true, false]
-// begin rotations after all have come into existene
+// begin rotations after all have come into existence
 setTimeout(function() {
   myAtoms.reverse();
   for (var j=0; j <= 117; j++) {
@@ -53,8 +70,11 @@ setTimeout(function() {
         orbitalRotationConfig.pattern.alternating = alternating
         alternating = alternate[getRandomInt(0,1)]
         orbitalRotationConfig.pattern.clockwise = alternating
+        var $container = $(`#bohr-container-${118 - j}`)
+        glow($container)
         myAtoms[j].rotateOrbitals(orbitalRotationConfig)
+        myAtoms[j].rotationPattern = orbitalRotationConfig // Store rotation pattern to show in UI
       }, j * 200)
     }(j));
   }
-}, speed * 118)
+}, speed * 130 )
